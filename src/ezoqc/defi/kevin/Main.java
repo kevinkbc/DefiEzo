07bc9debdb5084cd5a7fdd6e579d0d5836adfc77
kevinkbc <c.kevinbarros@gmail.com>
2020-08-19 12:54:59 -0400

    fix scanner

--- src/ezoqc/defi/kevin/Main.java
+++ src/ezoqc/defi/kevin/Main.java
@@ -1,5 +1,7 @@
 package ezoqc.defi.kevin;
 
+import java.util.Scanner;
+
 import ezoqc.defi.kevin.service.CalculatriceService;
 
 public class Main {
@@ -7,15 +9,26 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		CalculatriceService calculatriceService = new CalculatriceService();
-		String expression = "2.2 * 10 - 4 + 5 * 2";
-		try {
-			calculatriceService.creerArbreExpression(expression);
-			double resultat = calculatriceService.calculerArbre();
-			System.out.println("Resultat: "+ resultat);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		//String expression = "2.2 * 10 - 4 + 5 * 2";
+		Scanner in = new Scanner(System.in);
+		System.out.print("Veuillez ecrire l'expression: ");
+		String expression = in.nextLine();
+		while(expression != "") {
+			  
+	        
+			try {
+				calculatriceService.creerArbreExpression(expression);
+				double resultat = calculatriceService.calculerArbre();
+				System.out.println("Resultat: "+ resultat);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			System.out.print("Veuillez ecrire l'expression: ");
+			expression = in.nextLine();
 		}
+        
 	}
 
 }
--- src/ezoqc/defi/kevin/model/operation/Addition.java
+++ src/ezoqc/defi/kevin/model/operation/Addition.java
@@ -6,7 +6,9 @@ public class Addition implements IOperationBinaire{
 
 	@Override
 	public double calculer(double valeur1, double valeur2) {
-		return valeur1 + valeur2;
+		if(valeur1 == 1.0 && valeur2 == 1.0) {
+			return 1;
+		} else return valeur1 + valeur2;
 	}
 
 }
